f0d80ac5f5fdbfd559b59dcef7ea26c275b8c9bb
Mathieu Martin <webmat@gmail.com>
2008-10-07 18:33:52 -0400

    Added few tests, aligned them a bit

--- test/unit/array/includes_all_test.rb
+++ test/unit/array/includes_all_test.rb
@@ -9,15 +9,22 @@ class IncludesAllTest < Test::Unit::TestCase
     end
   end
   
-  test_includes_all :array => [], :includes_all? => [], :returns => true
-  test_includes_all :array => [], :includes_all? => [:a], :returns => false
+  test_includes_all :array => [],             :includes_all? => [], :returns => true
+  test_includes_all :array => [],             :includes_all? => [:a], :returns => false
+                                              
+  test_includes_all :array => [:a],           :includes_all? => [], :returns => true
+  test_includes_all :array => [:a],           :includes_all? => [:a], :returns => true
+  test_includes_all :array => [:a],           :includes_all? => [:a, :a], :returns => false
+  test_includes_all :array => [:a, :a],       :includes_all? => [:a], :returns => true
+  #test_includes_all :array => [:a, :a],       :includes_all? => [:a, :a], :returns => true
   
-  test_includes_all :array => [:a], :includes_all? => [], :returns => true
-  test_includes_all :array => [:a], :includes_all? => [:a], :returns => true
-  test_includes_all :array => [:a], :includes_all? => [:a, :a], :returns => false
-  test_includes_all :array => [:a, :a], :includes_all? => [:a], :returns => true
-  #test_includes_all :array => [:a, :a], :includes_all? => [:a, :a], :returns => true
+  test_includes_all :array => [:a, :b],       :includes_all? => [:a], :returns => true
+  test_includes_all :array => [:a, :b],       :includes_all? => [:a, :b], :returns => true
   
-  test_includes_all :array => [:a, :b], :includes_all? => [:a], :returns => true
+  test_includes_all :array => [[:a], [:b]],   :includes_all? => [[:a]], :returns => true
+  test_includes_all :array => [[:a], [:b]],   :includes_all? => [[:a], [:b]], :returns => true
   
+  test_includes_all :array => [[:a], [:b]],   :includes_all? => [:a, :b], :returns => false
+  test_includes_all :array => [[:a, :b]],     :includes_all? => [:a, :b], :returns => false
+  test_includes_all :array => [:a, :b],       :includes_all? => [[:a, :b]], :returns => false
 end
