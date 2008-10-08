7d99dd4c0b4ac9c4d3e1da4d0b6be88c43868223
Mathieu Martin <webmat@gmail.com>
2008-10-08 09:19:53 -0400

    Fixed includes_all

--- lib/mini_facet/array/includes_all.rb
+++ lib/mini_facet/array/includes_all.rb
@@ -1,12 +1,17 @@
-module MiniFacet
-  module IncludesAll
-    def includes_all?(ary)
-      # & is intersection
-      (self & ary) == ary
-    end
+module MiniFacet::IncludesAll
+  def includes_all?(ary)
+    our_count   = self.count_distinct
+    their_count = ary.count_distinct
     
-    alias :include_all? :includes_all? #Replicating Ruby's typos
+    their_count.each_pair do |k,v|
+      our_v = our_count[k]
+      return false if our_v.nil? || our_v < v
+    end
+    return true
   end
   
-  Array.send :include, IncludesAll
+  alias :include_all? :includes_all? #Replicating Ruby's typos
 end
+
+
+Array.send :include, MiniFacet::IncludesAll
--- test/unit/array/includes_all_test.rb
+++ test/unit/array/includes_all_test.rb
@@ -9,23 +9,23 @@ class ArrayIncludesAllTest < Test::Unit::TestCase
     end
   end
   
-  test_includes_all :array => [],             :includes_all? => [], :returns => true          # Same as Ruby's Set
-  test_includes_all :array => [],             :includes_all? => [:a], :returns => false
+  test_includes_all :array => [],             :includes_all? => [],           :returns => true    # Same as Ruby's Set
+  test_includes_all :array => [],             :includes_all? => [:a],         :returns => false
                                               
-  test_includes_all :array => [:a],           :includes_all? => [], :returns => true          # Same as Ruby's Set
-  test_includes_all :array => [:a],           :includes_all? => [:a], :returns => true
-  test_includes_all :array => [:a],           :includes_all? => [:a, :a], :returns => false
-  test_includes_all :array => [:a, :a],       :includes_all? => [:a], :returns => true
-  #test_includes_all :array => [:a, :a],       :includes_all? => [:a, :a], :returns => true
+  test_includes_all :array => [:a],           :includes_all? => [],           :returns => true    # Same as Ruby's Set
+  test_includes_all :array => [:a],           :includes_all? => [:a],         :returns => true
+  test_includes_all :array => [:a],           :includes_all? => [:a, :a],     :returns => false
+  test_includes_all :array => [:a, :a],       :includes_all? => [:a],         :returns => true
+  test_includes_all :array => [:a, :a],       :includes_all? => [:a, :a],     :returns => true
   
-  test_includes_all :array => [:a, :b],       :includes_all? => [:a], :returns => true
-  test_includes_all :array => [:a, :b],       :includes_all? => [:a, :b], :returns => true
-  test_includes_all :array => [:a, :b],       :includes_all? => [:b, :a], :returns => true
+  test_includes_all :array => [:a, :b],       :includes_all? => [:a],         :returns => true
+  test_includes_all :array => [:a, :b],       :includes_all? => [:a, :b],     :returns => true
+  test_includes_all :array => [:a, :b],       :includes_all? => [:b, :a],     :returns => true
   
-  test_includes_all :array => [[:a], [:b]],   :includes_all? => [[:a]], :returns => true
+  test_includes_all :array => [[:a], [:b]],   :includes_all? => [[:a]],       :returns => true
   test_includes_all :array => [[:a], [:b]],   :includes_all? => [[:a], [:b]], :returns => true
   
-  test_includes_all :array => [[:a], [:b]],   :includes_all? => [:a, :b], :returns => false
-  test_includes_all :array => [[:a, :b]],     :includes_all? => [:a, :b], :returns => false
-  test_includes_all :array => [:a, :b],       :includes_all? => [[:a, :b]], :returns => false
+  test_includes_all :array => [[:a], [:b]],   :includes_all? => [:a, :b],     :returns => false
+  test_includes_all :array => [[:a, :b]],     :includes_all? => [:a, :b],     :returns => false
+  test_includes_all :array => [:a, :b],       :includes_all? => [[:a, :b]],   :returns => false
 end
