6d955efd0724e9cd294cd0d080f6e0a08015df80
Mathieu Martin <webmat@gmail.com>
2008-10-07 21:13:13 -0400

    Tucked away my debugging crutches

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,6 @@ require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 
-#Optional
-require 'ruby-debug'
-require 'redgreen' unless ENV['TM_DIRECTORY'] #Don't load redgreen if running from TextMate
-
 $TEST_DIR = File.expand_path(File.dirname(__FILE__))
+
+require "#{$TEST_DIR}/my_test_helper" if File.exists?("#{$TEST_DIR}/my_test_helper.rb")
